163408085a131316c144cd7b88132c96ceed25c9
martinezfabiola <fabimartinezperez@gmail.com>
2017-10-04 21:51:14 -0400

    Mi primer commit
    Contiene:
    - el código base
    - suite de pruebas unitarias

---
+++ codeBase.py
@@ -0,0 +1,117 @@
+# Universidad Simon Bolivar
+# Laboratorio de Ingenieria de Software
+# Tarea 2
+# Autores:
+#   - Fabiola Martinez 1310838
+#   - Amanda Camacho
+# Descripcion: funcion que calcula el monto a pagar de un servicio.
+
+import sys
+import datetime
+import time
+from datetime import datetime, date, time, timedelta
+
+class Tarifa:
+    def __init__(self,semana,finSemana):
+        self.semana = semana
+        self.finSemana = finSemana
+        
+
+def calcularServicio(tarifa, tiempoServicio):
+    
+    tiempo = tiempoServicio[1] - tiempoServicio[0]
+    fechaInicio = tiempoServicio[0]
+    fechaFin = tiempoServicio[1]
+    #print(tiempo)
+
+    # Verificamos un servicio que dure menos de un dia completo dure mas de 15 min
+    try:
+    	assert( (fechaInicio.day!=fechaFin.day) or tiempo.seconds >= 900)
+    except:
+    	print("La duracion de un servicio debe ser mayr o ogual a 15 minutos\n")
+    	print("EL programa terminara")
+    	sys.exit()	
+
+    # Verificamos que el servicio no sea mayor a una semana
+    try:
+   		assert((tiempo.days!= 7 or tiempo.seconds==0) or tiempo.days < 7)
+    except:
+    	print("La duracion de un servicio debe ser menor o igual a siete dias\n")
+    	print("EL programa terminara")
+    	sys.exit()
+    
+    # Transformamos todo a segundos 
+    tiempoHora = tiempo.seconds//3600
+    tiempoMin = (tiempo.seconds%3600)*60
+    tiempoSeg = (tiempo.seconds%3600)
+
+    monto = 0
+    i=0
+
+    # Servicio dura un dia
+    if(tiempo.days == 0):
+
+    	# Verificamos que tarifa usar
+    	if(finDeSemana(fechaInicio)):
+    		tarif = tarifa.finSemana
+    	else:
+    		tarif = tarifa.semana
+    	# Calculamos el monto de acuerdo al tiempo trabajado 	
+    	if(tiempoHora == 0):
+            monto = tarif 
+    	else:
+            if (tiempoMin > 0):
+    			hora = tiempoHora + 1
+    			monto = tarif * hora
+            monto=tarif
+	
+	# Servicio dura mas de un dia   
+    else:
+    	if(fechaFin.hour < fechaInicio.hour):
+    		condicionParada = tiempo.days + 1
+
+    	elif(fechaInicio.hour == fechaFin.hour and fechaFin.second < fechaInicio.second):
+    		condicionParada = tiempo.days + 1
+
+    	else:
+    		condicionParada = tiempo.days
+
+    	while (i <= condicionParada): 
+    		# Verificamos que tarifa usar
+            if (finDeSemana(fechaInicio)):
+                tarif = tarifa.finSemana
+            else:
+                tarif =tarifa.semana
+
+            # Si el dia es el primero
+            if i == 0:
+                if fechaInicio.minute > 0 or fechaInicio.second > 0:
+                    monto += (24 - fechaInicio.hour) * tarif
+                else:
+                    monto += (24 - fechaInicio.hour) * tarif
+            # Si el dia es el ultimo del monto
+            elif i == condicionParada:
+                if fechaFin.minute > 0 or fechaFin.second > 0:
+                    monto += (fechaFin.hour + 1 ) * tarif
+                else:
+                    monto += fechaFin.hour * tarif
+            else:
+                monto += 24 * tarif
+               
+        
+            i += 1
+            
+            fechaInicio = fechaInicio + timedelta(days=1)
+
+    print monto
+    return monto
+
+def finDeSemana(fecha):
+    
+    dia=int(fecha.strftime("%w"))
+
+    if (dia == 6 or dia == 0):
+    
+        return True
+    
+    return False
---
+++ testSuite.py
@@ -0,0 +1,69 @@
+# Universidad Simon Bolivar
+# Laboratorio de Ingenieria de Software
+# Tarea 2
+# Autores:
+#	- Fabiola Martinez 1310838
+#	- Amanda Camacho
+# Descripcion: suite de pruebas unitarias.
+
+from codeBase import calcularServicio, finDeSemana, Tarifa
+import unittest
+import sys
+import datetime
+import time
+from datetime import datetime, date, time, timedelta
+
+class TestSuite(unittest.TestCase):
+
+	# Servicio dura 15 minutos
+	def testMinimo(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,2,20,10,0,0)
+		fechaFin = datetime(2017,2,20,10,15,0,0)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 10)
+
+	# Servicio dura 7 dias
+	def testMaximo(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,2,20,10,0,0)
+		fechaFin = datetime(2017,2,27,10,0,0)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 2160)
+
+	# Servicio dura 15 minutos mas un segundo
+	def testEsquinaInferior(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,2,20,10,0,0)
+		fechaFin = datetime(2017,2,20,10,15,01)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 10)
+
+
+	# Servicio dura 7 dias
+	def testEsquinaSuperior(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,2,20,10,00,0)
+		fechaFin = datetime(2017,2,27,9,59,59)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 2160)
+
+
+	# Serivicio inicia en un mes y termina en otro
+	def testMalicia1(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,9,30,10,00,0)
+		fechaFin = datetime(2017,10,2,10,00,0)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 860)
+
+
+	# Servicio incia en la madrugada de un dia y termina en la madrugada del otro
+	def testMalicia2(self):
+		tarifa = Tarifa(10,20)
+		fechaInicio = datetime(2017,10,1,00,00,0)
+		fechaFin = datetime(2017,10,2,00,00,0)
+		self.assertEqual(calcularServicio(tarifa, [fechaInicio,fechaFin]), 480)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
+
